e3e3e9ab5495d069b8646319aa9ec46d081589eb
Abhi <abhilashreddy571998@gmail.com>
2024-01-17 00:46:24 -0600

    Program to iterate the array

---
+++ src/TestApp.java
@@ -0,0 +1,8 @@
+public class TestApp {
+    public static void main(String[] args) {
+        int a [] = {44,2,34,23};
+        for(int ar : a){
+            System.out.println(ar);
+        }
+    }
+}
